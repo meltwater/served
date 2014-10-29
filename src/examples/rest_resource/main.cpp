67578a736b7a2593bf4b59d76bd4e1799ce47015
Christopher Gilbert <christopher.john.gilbert@gmail.com>
2014-10-29 20:37:39 +0000

    Added rest resource example

---
+++ src/examples/rest_resource/main.cpp
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2014 MediaSift Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <served/served.hpp>
+#include <served/request_error.hpp>
+#include <served/status.hpp>
+
+#include <iostream>
+
+#include <unistd.h>
+
+int main(int argc, char const* argv[])
+{
+	served::multiplexer mux;
+
+	mux.handle("/customers")
+		.get([](served::response & res, const served::request & req) {
+			// list customers
+		})
+		.post([](served::response & res, const served::request & req) {
+			// create customer
+		});
+	mux.handle("/customers/{id}")
+		.get([](served::response & res, const served::request & req) {
+			// read customer req.params["id"]
+		})
+		.put([](served::response & res, const served::request & req) {
+			// update customer req.params["id"]
+		})
+		.del([](served::response & res, const served::request & req) {
+			// delete customer req.params["id"]
+		});
+
+	served::net::server server("127.0.0.1", "8000", mux);
+	server.run(10);
+
+	return (EXIT_SUCCESS);
+}
